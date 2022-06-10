afaca7c019e7826bb9961a4ced909b677b8048d0
zach <zacharie.bugaud@grenoble-inp.org>
2022-06-10 12:03:39 +0200

    improve design

--- components/Nav.vue
+++ components/Nav.vue
@@ -2,10 +2,10 @@
 
 <ul class="flex justify-between">
   <li class="mr-3">
-    <a class="inline-block cursor-pointer border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4" v-on:click="MeRoute"  >Me</a>
+    <a class="inline-block text-3xl  cursor-pointer border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4" v-on:click="MeRoute"  >Me</a>
   </li>
   <li class="mr-3">
-    <a class="inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4" href="/search">Search</a>
+    <a class="inline-block text-3xl  border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4" href="/search">Search</a>
   </li>
 </ul>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
         <h1 class=" text-5xl font-bold" >Same goals</h1>
-        
+        -
         <h2>Platform to share our list of life goals and find similar people to connect with, through AI.</h2>
+        -
+        <img src="/list.png" width="100" height="100" />
+
     </div>
     
 </template>
